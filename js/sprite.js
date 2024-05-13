9352cd50f0bc7ca4be829b2ac32a1b2c3cc641a5
edgar <edgar99carbajal@gmail.com>
2024-05-12 17:20:31 -0700

    added yay

--- js/sprite.js
+++ js/sprite.js
@@ -26,6 +26,17 @@ class Sprite {
         this.imagesMap = map;
     }
 
+    yay(sprites){
+        for (let boid of sprites) {
+            if (boid !== this && dist(this.position.x, this.position.y, boid.position.x, boid.position.y) < 50) {
+                textSize(20);
+                fill(0);
+                text("Yay!", this.position.x + 40, this.position.y - 10);
+                break;
+            }
+        }
+    }
+
     align(sprites){
         let perceptionRadius = 100;
         let alignment = createVector();
@@ -118,6 +129,8 @@ class Sprite {
         let alignment = this.align(sprites);
         let cohesion = this.cohesion(sprites);
         let seperation = this.seperate(sprites);
+
+        this.yay(sprites);
         
         this.acceleration.add(alignment);
         this.acceleration.add(cohesion);
